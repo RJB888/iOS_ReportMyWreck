06a5ef18a3dd3630f36542903d8a7a4022d69a01
rjb888 <robert.j.bronson@gmail.com>
2021-10-21 16:29:47 -0700

    Added views for basic page layout

---
+++ report_my_wreck/report_my_wreck/EnterDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  EnterDetailsView.swift
+//  report_my_wreck
+//
+//  Created by ROBERT BRONSON on 10/21/21.
+//
+
+import SwiftUI
+
+struct EnterDetailsView: View {
+    var body: some View {
+        Text("Enter details for email")
+    }
+}
+
+struct EnterDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        EnterDetailsView()
+    }
+}
---
+++ report_my_wreck/report_my_wreck/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  report_my_wreck
+//
+//  Created by ROBERT BRONSON on 10/21/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Username \nPassword")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
