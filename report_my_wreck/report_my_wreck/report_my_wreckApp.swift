5d4864670e19e9f1e59dc05d170a4002597e713a
Richard <taerichard@gmail.com>
2021-11-17 15:05:28 -0800

    imported persistence controller and environment, created save and delete function for user context

--- report_my_wreck/report_my_wreck/report_my_wreckApp.swift
+++ report_my_wreck/report_my_wreck/report_my_wreckApp.swift
@@ -9,12 +9,28 @@ import SwiftUI
 
 @main
 struct report_my_wreckApp: App {
+    // into heirarchy 
     let persistenceController = PersistenceController.shared
+    
+    @Environment(\.scenePhase) var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
-    }
+        .onChange(of: scenePhase) {(newScenePhase) in
+            switch newScenePhase {
+            case .background:
+                print("scene in background")
+                //persistenceController.save()
+            case .inactive:
+                print("scene is inactive")
+            case .active:
+                print("scene is active")
+            @unknown default:
+                print("na")
+            }
+        }
+}
 }
