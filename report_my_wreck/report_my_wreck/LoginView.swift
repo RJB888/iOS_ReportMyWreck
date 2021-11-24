8937c64cffd7900b1ba786f730f9309c11644e8b
rjb888 <robert.j.bronson@gmail.com>
2021-11-24 12:30:22 -0800

    basic keychain login working

--- report_my_wreck/report_my_wreck/LoginView.swift
+++ report_my_wreck/report_my_wreck/LoginView.swift
@@ -9,25 +9,88 @@ import SwiftUI
 
 struct LoginView: View {
     @State private var userName: String = ""
-    @State private var password: String = ""
+    @State private var password: String = "!@##$%^&"
+    @State private var validated: Bool = false
+    @Binding var rootIsActive : Bool
+    @State private var badPassword: Bool = false
     
     var body: some View {
-        Form {
-            Section(header: Text("Username")){
-                TextField("USERNAME", text: $userName)
+        if getStoredPassword() == "" {
+            NavigationLink(destination: MapView(rootIsActive: $rootIsActive), isActive: $validated){}.hidden()
+            Form {
+                Section(header: Text("Set Password")){
+                    SecureField("PASSWORD", text: $password)
+                }
+                
+                Text("SET PASSWORD")
+                    .onTapGesture {
+                        updatePassword(password: password)
+                        validated = validatePassword(password: password)
+                    }
+                }
+        }
+        else {
+            NavigationLink(destination: MapView(rootIsActive: $rootIsActive), isActive: $validated){
+            }.hidden()
+            Form {
+                Section(header: Text("Enter Your Password")){
+                    SecureField("PASSWORD", text: $password)
+                }
+                Text("SIGN IN")
+                    .onTapGesture {
+                        //DELETE password for dev. purposes only
+                        if password == "..." {
+                            let kcw = KeychainWrapper()
+                            do {
+                                try kcw.deleteGenericPasswordFor(
+                                    account: "ReportMyWreck",
+                                    service: "unlockPassword")
+                            } catch {
+                                print("Error occurred while setting password")
+                            }
+                        }
+                        validated = validatePassword(password: password)
+                        if !validated {
+                            badPassword = true
+                        }
+                    }
+            }.alert(isPresented: $badPassword){
+                Alert(
+                    title: Text("PASSWORD ERROR"),
+                    message: Text("Incorrect password was entered. Enter '...' to reset."),
+                    dismissButton: .default(Text("Ok")))
             }
-            Section(header: Text("Password")){
-                SecureField("PASSWORD", text: $password)
-            }
-            Button(action:{}){
-                Text("SUBMIT")
-            }
+        }
+    }
+    
+    func validatePassword(password: String) -> Bool{
+        return getStoredPassword() == password
+    }
+    func getStoredPassword() -> String{
+        let kcw = KeychainWrapper()
+        if let storedPassword = try? kcw.getGenericPasswordFor(
+            account: "ReportMyWreck",
+            service: "unlockPassword"){
+            return storedPassword
+        }
+        return ""
+    }
+    func updatePassword(password: String){
+        let kcw = KeychainWrapper()
+        do {
+            try kcw.storeGenericPasswordFor(
+                account: "ReportMyWreck",
+                service: "unlockPassword",
+                password: password)
+        } catch {
+            print("Error occurred while setting password")
         }
     }
 }
 
+
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        LoginView(rootIsActive: .constant(false))
     }
 }
